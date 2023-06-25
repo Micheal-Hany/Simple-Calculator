814c3e40256a9de2f71d756aa1398213e0af188f
micheal00000 <michealhany991@gmail.com>
2023-06-25 23:01:48 +0300

    first commit

---
+++ lib/buttons.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+// creating Stateless Widget for buttons
+class MyButton extends StatelessWidget {
+// declaring variables
+  final color;
+  final textColor;
+  final String buttonText;
+  final buttontapped;
+
+//Constructor
+  MyButton(
+      {this.color,
+      this.textColor,
+      required this.buttonText,
+      this.buttontapped});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: buttontapped,
+      child: Padding(
+        padding: const EdgeInsets.all(0.2),
+        child: ClipRRect(
+          
+          child: Container(
+            margin: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+                color: color, borderRadius: BorderRadius.circular(50)),
+            child: Center(
+              child: Text(
+                buttonText,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
